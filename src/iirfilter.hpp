47b73cfb2b122c0aacb73789bad616d1c2531beb
Garten Schlauch <gartenschlauchify@gmail.com>
2015-06-19 00:08:40 +0200

    basic structure works now

--- src/iirfilter.hpp
+++ src/iirfilter.hpp
@@ -4,9 +4,13 @@
 #include <dspxx.h>
 
 template<typename float_type>
-class iirfilter: filter<float_type>
+class iirfilter: public filter<float_type>
 {
 public:
+    float_type processSample(float_type sample)
+    {
+        return 0.0;
+    }
     float_type * state;
     float_type * nominator_coefficients;
     float_type * denominator_co;
--- test/main.c
+++ test/main.c
@@ -11,18 +11,24 @@ int main(int argc, char**argv)
     printf("=== test main ===\n");
 
     size_t n = 10;
-	size_t n_out = n;
+    size_t n_out = n;
     float_type input_samples[n];
-	float_type output_samples[n];
+    float_type output_samples[n];
     for (size_t k = 0; k < n; k++)
     {
         input_samples[k] = exp(-k) * (1 - 2*(k%2));
     }
 
-    iirfilter<float_type> f();
-    f.processSamples(n, input_samples, n_out, output_samples);
+    iirfilter<float_type> f;
+    size_t k_out = f.processSamples(n, input_samples, n_out, output_samples);
 
+    printf("processed %d samples\n", k_out);
+    for (size_t k = 0; k < k_out; k++)
+    {
+        printf("sample[%d] == %f\n", k, output_samples[k]);
+    }
 
+    printf("=== done ===\n");
     return 0;
 }
 
